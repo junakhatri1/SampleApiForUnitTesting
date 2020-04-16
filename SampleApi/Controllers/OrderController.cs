f082f26b97925563e7282e1ec5f90a86f1256a47
Juna <junakhatri@gmail.com>
2020-04-16 20:12:22 +0000

    Add project files.

---
+++ SampleApi/Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApi.Controllers
+{
+    public class OrderController : Controller
+    {
+        IEstimateshipping ShippingEstimator;
+
+        public OrderController(IEstimateshipping shippingEstimator)
+        {
+            ShippingEstimator = shippingEstimator;
+        }
+
+        [HttpPost("/orders")]
+        public IActionResult PlaceOrder([FromBody] Order order)
+        {
+
+            //Validate it 
+            //save it to the db
+
+            var response = new OrderResponse
+            {
+                For = order.For,
+                Item = order.Item,
+                Qty = order.Qty,
+                ExpectedShipDate = DateTime.Now //RED FLAGS 
+            };
+            return Ok(response);
+        }
+    }
+}
+public class Order
+{
+    public string For { get; set; }
+    public string Item { get; set; }
+    public int Qty { get; set; }
+}
+
+public class OrderResponse
+{
+    public string For { get; set; }
+    public string Item { get; set; }
+    public int Qty { get; set; }
+    public DateTime ExpectedShipDate { get; set; }
+}
---
+++ SampleApi/Controllers/StandardShippingEstimator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace SampleApi.Controllers
+{
+    public class StandardShippingEstimator : IEstimateshipping
+    {
+        ISystemTime SystemTime;
+        public StandardShippingEstimator(ISystemTime systemTime)
+        {
+            SystemTime = systemTime;
+        }
+
+        DateTime IEstimateshipping.GetEstimatedShipDate()
+        {
+            return SystemTime.GetCurrent().Hour <= 17 ?
+                SystemTime.GetCurrent().Date :
+                SystemTime.GetCurrent().AddDays(1).Date;
+
+        }
+    }
+}
